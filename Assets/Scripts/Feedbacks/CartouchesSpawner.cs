ad53f9b1243f7d154fce8ff8413dced3fdf3ed0a
VSteeph <autresvv@gmail.com>
2020-11-14 18:58:38 +0100

    OnShot Feedback done

--- Assets/Scripts/Feedbacks/CartouchesSpawner.cs
+++ Assets/Scripts/Feedbacks/CartouchesSpawner.cs
@@ -8,17 +8,20 @@ public class CartouchesSpawner : MonoBehaviour
     [SerializeField] private GameObject Cartouche;
     [Header("Refs")]
     [SerializeField] private GunAim aim;
-    [SerializeField] private AvatarManager avatarManager;
+    [SerializeField] private CharacterManager characterManager;
+    [SerializeField] private float cartoucheTravelCoeff;
+    [SerializeField] private Transform bulletSpawnPoint;
 
-    void Awake()
+    void Start()
     {
-        avatarManager.onShot += SpawnCartouche;
+        characterManager.onShot += SpawnCartouche;
     }
 
     public void SpawnCartouche()
     {
-        Cartouches cart = Instantiate(this.Cartouche, this.transform.position, Quaternion.identity).GetComponent<Cartouches>();
-        Vector3 dir = aim.aim.position - this.transform.position;
+        Cartouches cart = Instantiate(this.Cartouche, this.bulletSpawnPoint.position, Quaternion.identity).GetComponent<Cartouches>();
+        cart.force = cartoucheTravelCoeff;
+        Vector3 dir = aim.aim.position - this.bulletSpawnPoint.position;
         if(dir.x >= 0) cart.AimDir = new Vector2(-Random.value,Random.value);
         else cart.AimDir = new Vector2 (Random.value, Random.value);
     }
