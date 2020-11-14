0c80861c05205c3557d2f9be6cefd22294c4f8b3
LahlouMcV <74438415+LahlouMcV@users.noreply.github.com>
2020-11-14 19:01:03 +0100

    What push

--- Assets/Scripts/Feedbacks/Cartouches.cs
+++ Assets/Scripts/Feedbacks/Cartouches.cs
@@ -7,10 +7,12 @@ public class Cartouches : MonoBehaviour
     [Header("Refs")]
     [SerializeField] private Rigidbody2D rb;
     public Vector2 AimDir;
+    public float force;
 
     void Start()
     {
-        rb.AddForce(new Vector2(-AimDir.x, Mathf.Abs(AimDir.y)+1).normalized * 5, ForceMode2D.Impulse);
+        rb.AddForce(new Vector2(-AimDir.x, Mathf.Abs(AimDir.y)+1).normalized * force, ForceMode2D.Impulse);
+        rb.AddTorque(Random.value * force * 200);
         Invoke("StopRigidbody", 1);
     }
 
@@ -19,6 +21,7 @@ public class Cartouches : MonoBehaviour
         rb.isKinematic = true;
         rb.gravityScale = 0;
         rb.velocity = Vector2.zero;
+        rb.angularVelocity = 0f;
         Invoke("TimeToDie", 1);
     }
 
