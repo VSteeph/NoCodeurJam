879d70e343c6000b80d4f650f619dfac1fa08964
LahlouMcV <74438415+LahlouMcV@users.noreply.github.com>
2020-11-14 16:38:09 +0100

    Music Added

---
+++ Assets/Scripts/Singleton/SoundSingleton.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+
+public class SoundSingleton : MonoBehaviour
+{
+    [Header("Stats")]
+    [SerializeField] CharacterManager characterManager;
+    [Header("AvatarSounds")]
+    [EventRef]
+    public string ShootEvent = "";
+    private  FMOD.Studio.EventDescription ShootEventDescription;
+    [EventRef]
+    public string DodgeEvent = "";
+    private  FMOD.Studio.EventDescription DodgeEventDescription;
+    [EventRef]
+    public string HitEvent = "";
+    private  FMOD.Studio.EventDescription HitEventDescription;
+    [Header("RobotSounds")]
+    [EventRef]
+    public string RobotIdle = "";
+    private  FMOD.Studio.EventDescription RobotIdleDescription;
+    [EventRef]
+    public string RobotHit = "";
+    private  FMOD.Studio.EventDescription RobotHitDescription;
+    [EventRef]
+    public string RobotDeath = "";
+    private  FMOD.Studio.EventDescription RobotDeathDescription;
+
+    void Start() 
+    {
+        characterManager.onShot+=PlayShoot;
+        Lookup();
+    }
+
+
+    void Lookup()
+    {
+        ShootEventDescription = RuntimeManager.GetEventDescription(ShootEvent);
+        DodgeEventDescription = RuntimeManager.GetEventDescription(DodgeEvent);
+        HitEventDescription = RuntimeManager.GetEventDescription(HitEvent);
+        RobotIdleDescription = RuntimeManager.GetEventDescription(RobotIdle);
+        RobotHitDescription = RuntimeManager.GetEventDescription(RobotHit);
+        RobotDeathDescription = RuntimeManager.GetEventDescription(RobotDeath);
+    }
+
+    public void PlayShoot()
+    {
+        RuntimeManager.PlayOneShot(ShootEvent);
+    }
+
+    public void PlayDodge()
+    {
+        RuntimeManager.PlayOneShot(DodgeEvent);
+    }
+
+    public void PlayHit()
+    {
+        RuntimeManager.PlayOneShot(HitEvent);
+    }
+
+    public void PlayRobotHit()
+    {
+        RuntimeManager.PlayOneShot(RobotHit);
+    }
+
+    public void PlayRobotDeath()
+    {
+        RuntimeManager.PlayOneShot(RobotDeath);
+    }
+}
