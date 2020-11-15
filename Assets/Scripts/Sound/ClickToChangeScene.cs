c0b5c2afa3926471f7332c707d5bdfbf59e603d1
LahlouMcV <74438415+LahlouMcV@users.noreply.github.com>
2020-11-15 11:38:39 +0100

    System Feedback Implimented

---
+++ Assets/Scripts/Sound/ClickToChangeScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickToChangeScene : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.anyKeyDown)
+        {
+            CIvEnergyManager.cIvEnergyManager.Reset();
+            CIvEnergyManager.cIvEnergyManager.BackToChoiceMenu();
+        }
+    }
+}
