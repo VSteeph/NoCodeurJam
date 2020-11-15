0e53ca41eb41b8614b0429297bde1c385b2dbc1a
LahlouMcV <74438415+LahlouMcV@users.noreply.github.com>
2020-11-15 13:13:40 +0100

    Merge

--- Assets/Scripts/CIvEnergyManager.cs
+++ Assets/Scripts/CIvEnergyManager.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using System.Reflection;
 
 public class CIvEnergyManager : MonoBehaviour
 {
@@ -15,8 +17,33 @@ public class CIvEnergyManager : MonoBehaviour
     public int CompletedLevels;
     private bool MainMenu;
     private bool Lost;
+
+    //Upgrade
+    [HideInInspector] public float speed;
+    [SerializeField] public float speedPlayerIncrease;
+    [HideInInspector] public int health;
+    [SerializeField] public int hpPlayerIncrease;
+    [HideInInspector] public float lifeDurationBullet;
+    [SerializeField] public float lifeDurationBulletIncrease;
+    [HideInInspector] public float speedBullet;
+    [SerializeField] public float speedBulletIncrease;
+    [HideInInspector] public int damageBullet;
+    [SerializeField] public int damageBulletIncrease;
+    [HideInInspector] public float shootCooldown;
+    [SerializeField] public float shootCooldownIncrease;
+    [HideInInspector] public float dodgeCooldown;
+    [SerializeField] public float dodgeCooldownIncrease;
+    [HideInInspector] public int dodgeDuration;
+    [SerializeField] public int dodgeDurationIncrease;
+    [HideInInspector] public int pushBackOnHit;
+    [SerializeField] public int pushBackMonsterIncrease;
+
+    private Dictionary<string, object> UpgradeWithBasedIncrease;
+    private System.Random rand = new System.Random();
+
     void Awake()
     {
+        GenerateUpgrade();
         MainMenu = true;
         Reset();
         if(CIvEnergyManager.cIvEnergyManager != null) Destroy(this.gameObject);
@@ -25,6 +52,21 @@ public class CIvEnergyManager : MonoBehaviour
             DontDestroyOnLoad(this.gameObject);
             CIvEnergyManager.cIvEnergyManager = this;
         }
+    }
+
+    private void GenerateUpgrade()
+    {
+        UpgradeWithBasedIncrease = new Dictionary<string, object>();
+        UpgradeWithBasedIncrease.Add("speed", speedPlayerIncrease);
+        UpgradeWithBasedIncrease.Add("health", hpPlayerIncrease);
+        UpgradeWithBasedIncrease.Add("lifeDurationBullet", lifeDurationBulletIncrease);
+        UpgradeWithBasedIncrease.Add("speedBullet", speedBulletIncrease);
+        UpgradeWithBasedIncrease.Add("damageBullet", damageBulletIncrease);
+        UpgradeWithBasedIncrease.Add("shootCooldown", shootCooldownIncrease);
+        UpgradeWithBasedIncrease.Add("dodgeCooldown", dodgeCooldownIncrease);
+        UpgradeWithBasedIncrease.Add("dodgeDuration", dodgeDurationIncrease);
+        UpgradeWithBasedIncrease.Add("pushBackOnHit", pushBackMonsterIncrease);
+
     }
 
     public void ConsumeEnergy(int amount)
@@ -115,5 +157,83 @@ public class CIvEnergyManager : MonoBehaviour
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene(11);
         Lost = true;
+    }
+
+    public void Upgrade()
+    {
+        var increase = rand.Next(0, UpgradeWithBasedIncrease.Count);
+        var element = UpgradeWithBasedIncrease.ElementAt(increase);
+        var parameters = this.GetType().GetField(element.Key);
+        var oldValue = parameters.GetValue(this);
+
+        if (element.Value is int)
+        {
+            var newValue = (int)parameters.GetValue(this) + (int)element.Value;
+            parameters.SetValue(this, newValue);
+            Debug.Log(element.Key + " was upgradezd from " + oldValue + " to " + newValue);
+        }
+        else
+        {
+            var newValue = (float)parameters.GetValue(this) + (float)element.Value;
+            parameters.SetValue(this, newValue);
+        }
+    }
+
+    public void SyncCharacterStats(PlayerManager player, RobotManager robot)
+    {
+        foreach (KeyValuePair<string, object> entry in UpgradeWithBasedIncrease)
+        {
+            Debug.Log("update");
+            switch (entry.Key)
+            {
+                case ("speed"):
+                    player.speed += (float)GetValue(entry.Key);
+                    break;
+
+                case ("health"):
+                    player.currentHealth += (int)GetValue(entry.Key);
+                    break;
+
+                case ("lifeDurationBullet"):
+                    player.loadedBullet.lifeDuration += (float)GetValue(entry.Key);
+                    break;
+
+                case ("speedBullet"):
+                    player.loadedBullet.speed += (float)GetValue(entry.Key);
+                    break;
+
+                case ("damageBullet"):
+                    player.loadedBullet.damage += (int)GetValue(entry.Key);
+                    break;
+
+                case ("shootCooldown"):
+                    player.shotCooldown += (float)GetValue(entry.Key);
+                    break;
+
+                case ("dodgeCooldown"):
+                    player.dodgeCooldown += (float)GetValue(entry.Key);
+                    break;
+
+                case ("dodgeDuration"):
+                    player.dodgeDurationInFrames += (int)GetValue(entry.Key);
+                    break;
+
+                case ("pushBackOnHit"):
+                    robot.SyncEveryRobotPushBack((float)GetValue(entry.Key));
+                    break;
+
+
+                default:
+                    break;
+            }
+
+        }
+    }
+
+
+    public object GetValue(string key)
+    {
+        var parameters = this.GetType().GetField(key);
+        return parameters.GetValue(this);
     }
 }
